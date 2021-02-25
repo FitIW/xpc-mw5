5ebf1427b8655ab83a4fe507665c8d9a47cd6fe6
Patrik Švikruha <patrik.svikruha@gmail.com>
2021-02-25 20:20:05 +0100

    Add Labs 001

---
+++ Labs/001_ProjectSystem_References/MyPerfectProject/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using ClassLibrary1;
+using MyPerfectProject.Folder;
+
+namespace MyPerfectProject
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var i = 5;
+            {
+                var ii = 44;
+                i = 8;
+            }
+
+            //var @class = "adssa";
+            var test = new Test();
+            var class1 = new Class1();
+            var myProgramClass2 = new MyProgramClass2();
+            //var myClass = new Class1.MyClass();
+
+            // not allowed call non static member from static context
+            // MyMethod(5);
+        }
+
+        public void MyMethod(int age)
+        {
+            //calling static member in scope
+            Main(new[] {"asd"});
+        }
+    }
+
+    internal class MyProgramClass2
+    {
+        public MyProgramClass2()
+        {
+            //calling static member of class program
+            var arguments = new[] {"asd"};
+            Program.Main(arguments);
+        }
+    }
+}

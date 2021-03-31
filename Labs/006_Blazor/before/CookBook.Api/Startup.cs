c2227321390c0b410818bca62ff047b0c11b2667
Patrik Švikruha <patrik.svikruha.dev@gmail.com>
2021-03-31 19:20:35 +0200

    Add labs 4 before

---
+++ Labs/006_Blazor/before/CookBook.Api/Startup.cs
@@ -0,0 +1,64 @@
+using AutoMapper;
+using CookBook.BL.Api.Installers;
+using CookBook.DAL.Installers;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NSwag.AspNetCore;
+
+namespace CookBook.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers()
+                .AddNewtonsoftJson()
+                .AddFluentValidation(configuration =>
+                    configuration.RegisterValidatorsFromAssemblyContaining<BLApiInstaller>());
+
+            services.AddLocalization(options => options.ResourcesPath = string.Empty);
+
+            services.AddOpenApiDocument();
+
+            new DALInstaller().Install(services);
+            new BLApiInstaller().Install(services);
+
+            services.AddAutoMapper(typeof(DALInstaller), typeof(BLApiInstaller));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IMapper mapper)
+        {
+            mapper.ConfigurationProvider.AssertConfigurationIsValid();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseOpenApi();
+                app.UseSwaggerUi3(settings =>
+                {
+                    settings.SwaggerRoutes.Add(new SwaggerUi3Route("v1.0", "/swagger/v1/swagger.json"));
+                });
+            }
+           
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>endpoints.MapControllers());
+
+         
+        }
+    }
+}
